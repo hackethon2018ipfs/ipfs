9410896d11d7e405c72d54225f3e23692f06d84d
jimmy collazos <jimmy.collazos.utrilla@bbva.com>
2018-03-03 13:13:39 +0100

    add ContentStore contract

---
+++ contracts/ContentStore.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.19;
+
+contract ContetStore {
+  mapping (address => string) public previewContents;
+  mapping (address => string) private mainContents;
+
+  function ContentStore () public {
+    addContent(
+      "Qmdw1pN4MqPts76bo1RZ4wgJ6REHcRDP9drpYbk14TdWte",
+      "QmX4U7jwgYfM6EzMt3SiiEL24EcSBLc9hKWSwnqqb19phR"
+      );
+  }
+
+  function addContent (string _preview, string _main) private {
+    previewContents[msg.sender] = _preview;
+    mainContents[msg.sender] = _main;
+    // TODO: fire event
+  }
+
+  function getMain (address _owner) public view returns (string) {
+    return mainContents[_owner];
+  }
+}
